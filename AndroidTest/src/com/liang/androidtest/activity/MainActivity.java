a47ffe84327eaef88c2a9c50b09d253d40699f17
superliang <super_liang@sina.cn>
2014-06-13 17:10:58 +0800

    添加了ViewPager内容
    添加了viewpager功能

--- AndroidTest/src/com/liang/androidtest/activity/MainActivity.java
+++ AndroidTest/src/com/liang/androidtest/activity/MainActivity.java
@@ -1,24 +1,47 @@
 package com.liang.androidtest.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.liang.androidtest.R;
 
 public class MainActivity extends Activity {
-
+	private ListView listView;
+	private List<String> adapterList= new ArrayList<String>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		
+		listView = (ListView) findViewById(R.id.listview);
+		
+		adapterList.add("ViewPager控件");
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, adapterList);
+		
+		listView.setAdapter(adapter);
+		
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				Intent intent = new Intent();
+				if(arg2==0){
+					intent.setClass(MainActivity.this, ViewPagerActivity.class);
+					startActivity(intent);
+				}
+			}
+		});
 	}
 
 }
